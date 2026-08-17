c12a5667772630bf1c8f6df825dbd728abefe743
taozibb23 <295817059@qq.com>
2026-08-17 22:26:35 +0800

    feat: 灯带检测+配对的bool判断函数 目前还在调试阶段还没弄好配对一直失败，筛选条件存疑，灯带的条件和对应颜色范围的筛选，还有配对的条件，框架完整了，还得搞清楚从打印的角度去改配对的条件

--- armor_detect.cpp
+++ armor_detect.cpp
@@ -1,6 +1,6 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
-
+#include <cmath>
 // 阈值：trackbar 调参结果（写死）
 int hminb = 72,  sminb = 16,  vminb = 255;
 int hmaxb = 101, smaxb = 69,  vmaxb = 255;
@@ -17,6 +17,46 @@ void onMouse(int event, int x, int y, int flags, void* userdata) {
                   << " S=" << (int)p[1] << " V=" << (int)p[2] << std::endl;
     }
 }
+double normalizeDeg(double angle){//灯带配对
+    while(angle > 180.0) angle -= 360.0;
+    while(angle < -180.0) angle += 360.0;
+    return angle;
+}
+bool isValidPair(const cv::RotatedRect& a,const cv::RotatedRect& b){
+    auto heightratio = a.size.height/b.size.height;
+    if (heightratio < 0.67 || heightratio > 1.5)return false;//高度比例  if早退模式里面写反条件
+         double a_dir = normalizeDeg(a.angle + 90);
+         double b_dir = normalizeDeg(b.angle + 90);
+         if(std::abs(a_dir - b_dir) > 15)return false;
+            if(std::abs(normalizeDeg(a.angle) - normalizeDeg(b.angle)) > 15)return false; //绝对值  std  abs
+            std::cout<<"angle : "<<a.angle<<"   "<<b.angle<<std::endl;
+            auto centerspacing_x = a.center.x - b.center.x;//间距是负数怎么办
+            auto centerspacing_y = a.center.y - b.center.y;
+            auto centerdis = std::hypot(centerspacing_x,centerspacing_y);
+            //计算间距
+            auto averageheight = (a.size.height + b.size.height) / 2 ;
+            if(4 < centerdis / averageheight || centerdis / averageheight < 1.5)return false;//中心间距除以平均u高度来筛选
+               auto dx = a.center.x - b.center.x;
+               auto dy = a.center.y - b.center.y;
+               auto center_angle_rad = std::atan2(dy,dx);
+               auto center_angle_deg = center_angle_rad *180 / CV_PI;  //弧度转度数
+               auto judgmentangle = 90; //......可能不要
+               std::cout<<" angle :"<<center_angle_deg<<std::endl;
+               auto angle_a = (a.angle + 90.0); //angle是nn短边的夹角+90nh变成aa长边了
+               auto angle_b = (b.angle + 90.0);
+               auto two_bar_deg = (angle_a + angle_b) / 2;//取得平均值
+               two_bar_deg = normalizeDeg(two_bar_deg);
+               center_angle_deg = normalizeDeg(center_angle_deg);
+               auto bar_center_dif = two_bar_deg - center_angle_deg;
+               bar_center_dif = normalizeDeg(bar_center_dif);
+               auto deviation = std::abs(std::abs(bar_center_dif) - 90.0);//与垂直的偏差
+               float tolerance_deg = 20.0;//+-  相差范围是20
+                    if(deviation > tolerance_deg)return false;//中心连线和灯带的角度差
+                        std::cout<<"配对成功"<<std::endl;
+                        return true;                                                
+   
+}
+
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
@@ -51,30 +91,77 @@ int main(int argc, char* argv[]) {
     std::vector<cv::Vec4i> hierarcy;
     cv::Mat boxPtsMat; 
     std::vector<cv::Point> boxPtsInt;
+    std::vector<cv::RotatedRect> lightBars;
     cv::findContours(imgmask, contours,hierarcy,cv::RETR_EXTERNAL,cv::CHAIN_APPROX_SIMPLE);
+    
     for(int i = 0;i <contours.size(); i++){
         double area = cv::contourArea(contours[i]);
         std::cout<<"contours  :  "<<i<<"area  :  "<<area<<std::endl;
-        if (cv::contourArea(contours[i]) > 30 ){
+        if (cv::contourArea(contours[i]) > 30 && cv::contourArea(contours[i]) < 1000){
             cv::RotatedRect rRect = cv::minAreaRect(contours[i]);
+            double h = rRect.size.height, w = rRect.size.width;
+            if(h < w)std::swap(h,w);
+            if(h / w < 3 )continue;//长宽比
             cv::boxPoints(rRect,boxPtsMat);
             std::vector<cv::Point2f> boxPts;
+            lightBars.push_back(cv::minAreaRect(contours[i]));
             for( int k = 0; k < boxPtsMat.rows; k++){
                 boxPts.push_back(cv::Point2f(boxPtsMat.at<float>(k,0),
-                                 boxPtsMat.at<float>(k,1)));
+                                             boxPtsMat.at<float>(k,1)));
             }
             boxPtsInt.clear();
             for(int k = 0; k<4;k++){
                 boxPtsInt.push_back(cv::Point(boxPts[k].x, boxPts[k].y));
             }
             cv::polylines(img,boxPtsInt,true,cv::Scalar(0,255,0), 2);
+            if(lightBars.empty()){std::cout<<" 没有检测到灯带 "<<std::endl;}
             std::cout<<"灯带角度"<<rRect.angle<<"灯带中心"<<rRect.center<<std::endl;
+            std::cout<<"检测到灯带： "<<lightBars.size() << " 个 "<<std::endl;
         }
+     
     }
+           //----------------------筛选灯带数量----------------
+            std::cout<<"正在配对灯带中....."<<std::endl;
+            for(std::size_t i = 0; i < lightBars.size(); i++){
+                for(std::size_t j = i + 1 ; j < lightBars.size(); j++){//i为第一个e灯带j为第二个灯带
+                    if(isValidPair(lightBars[i],lightBars[j])){
+                        //配对成功
+                        std::cout<<"配对 成功 "<<i<<" + "<<j<<std::endl;
+                        cv::Mat mi,mj;
+                        cv::boxPoints(lightBars[i],mi);
+                        cv::boxPoints(lightBars[j],mj);//获取灯带的四个点
+                        std::vector<cv::Point2f> allPts;
+                        for(int i = 0;i < mi.rows; i++){//找出mi的列数 这里的mat是4x2的矩阵
+                            allPts.push_back(cv::Point2f(mi.at<float>(i,0),mi.at<float>(i,1)));
+                        }
+                        for(int j = 0;j < mj.rows; j++){
+                            allPts.push_back(cv::Point2f(mj.at<float>(j,0),mj.at<float>(j,1)));
+                        }
+                        auto armorpts = cv::minAreaRect(allPts);//i最小矩形
+                        cv::Mat armorPtsMat;//用math接受后面转换成int
+                        cv::boxPoints(armorpts,armorPtsMat);
+                        std::vector<cv::Point> armorPtsInt;//储存int类型的
+                        for(int i = 0;i < armorPtsMat.rows;i++){
+                            float x = armorPtsMat.at<float>(i,0);
+                            float y = armorPtsMat.at<float>(i,1);
+                            armorPtsInt.push_back(cv::Point(cvRound(x),cvRound(y)));
+                            
+                        }
+                        if(armorPtsInt.empty()){std::cout<<" 没有装甲板可以绘制"<<std::endl;}
+                        cv::polylines(img,armorPtsInt,true,cv::Scalar(0,255,0),2,cv::LINE_AA);
+                    }else std::cout<<"配对失败"<<std::endl;
 
-    cv::imshow("img", img);
-    cv::setMouseCallback("img", onMouse, &imgHSV);
-    cv::imshow("imgmask", imgmask);
-    cv::waitKey(0);
-    return 0;
+                }
+            
+            }
+
+       
+    
+   
+     cv::imshow("img", img);
+     cv::setMouseCallback("img", onMouse, &imgHSV);
+     cv::imshow("imgmask", imgmask);
+     cv::waitKey(0);
+     return 0;
+
 }
